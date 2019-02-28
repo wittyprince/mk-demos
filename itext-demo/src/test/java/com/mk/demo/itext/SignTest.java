4438f09a3d1a3d0b0ed0bbf5710ce9ebd4a7ca38
wangchen <516760573@qq.com>
2019-02-28 09:40:05 +0800

    itext-demo

--- itext-demo/src/test/java/com/mk/demo/itext/SignTest.java
+++ itext-demo/src/test/java/com/mk/demo/itext/SignTest.java
@@ -1,36 +1,53 @@
 package com.mk.demo.itext;
 
+import com.itextpdf.forms.PdfAcroForm;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDate;
+import com.itextpdf.kernel.pdf.PdfDictionary;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfName;
+import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfString;
+import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.StampingProperties;
+import com.itextpdf.kernel.pdf.annot.PdfAnnotation;
 import com.itextpdf.signatures.BouncyCastleDigest;
 import com.itextpdf.signatures.CertificateUtil;
 import com.itextpdf.signatures.DigestAlgorithms;
 import com.itextpdf.signatures.IExternalDigest;
 import com.itextpdf.signatures.IExternalSignature;
 import com.itextpdf.signatures.ITSAClient;
+import com.itextpdf.signatures.PdfPKCS7;
 import com.itextpdf.signatures.PdfSignatureAppearance;
 import com.itextpdf.signatures.PdfSigner;
 import com.itextpdf.signatures.PrivateKeySignature;
+import com.itextpdf.signatures.SignaturePermissions;
+import com.itextpdf.signatures.SignatureUtil;
 import com.itextpdf.signatures.TSAClientBouncyCastle;
+import org.bouncycastle.jcajce.provider.asymmetric.x509.CertificateFactory;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
 import java.security.PrivateKey;
 import java.security.Security;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -134,4 +151,121 @@ public class SignTest {
         return new TSAClientBouncyCastle(tsaUrl);
     }
 
+    @Test
+    public void tesInspectSignatures() throws IOException, GeneralSecurityException {
+        String path = "D:/tmp/hello2.pdf";
+        PdfDocument pdfDoc = new PdfDocument(new PdfReader(path));
+        PdfAcroForm form = PdfAcroForm.getAcroForm(pdfDoc, false);
+        SignaturePermissions perms = null;
+        SignatureUtil signUtil = new SignatureUtil(pdfDoc);
+        List<String> names = signUtil.getSignatureNames();
+        for (String name : names) {
+            System.out.println("===== " + name + " =====");
+            perms = inspectSignature(pdfDoc, signUtil, form, name, perms);
+        }
+        System.out.println();
+    }
+
+    private SignaturePermissions inspectSignature(PdfDocument pdfDoc, SignatureUtil signUtil, PdfAcroForm form, String name, SignaturePermissions perms) throws GeneralSecurityException, IOException {
+        if (form.getField(name).getWidgets() != null && form.getField(name).getWidgets().size() > 0) {
+            int pageNum = 0;
+            Rectangle pos = form.getField(name).getWidgets().get(0).getRectangle().toRectangle();
+            for (int i = 1; i <= pdfDoc.getNumberOfPages(); i++) {
+                PdfPage page = pdfDoc.getPage(i);
+                for (PdfAnnotation annot : page.getAnnotations()) {
+                    System.out.println();
+                    if (PdfName.Sig.equals(annot.getPdfObject().get(PdfName.FT)) && name.equals(annot.getPdfObject().get(PdfName.T).toString())) {
+                        pageNum = pdfDoc.getPageNumber(page);
+                        break;
+                    }
+                }
+            }
+            if (pos.getWidth() == 0 || pos.getHeight() == 0) {
+                System.out.println("Invisible signature");
+            } else {
+                System.out.println(String.format("Field on page %s; llx: %s, lly: %s, urx: %s; ury: %s", pageNum, pos.getLeft(), pos.getBottom(), pos.getRight(), pos.getTop()));
+            }
+        }
+
+        /*PdfPKCS7 pkcs7 = verifySignature(signUtil, name);
+        System.out.println("Digest algorithm: " + pkcs7.getHashAlgorithm());
+        System.out.println("Encryption algorithm: " + pkcs7.getEncryptionAlgorithm());
+        System.out.println("Filter subtype: " + pkcs7.getFilterSubtype());
+        X509Certificate cert = (X509Certificate) pkcs7.getSigningCertificate();
+        System.out.println("Name of the signer: " + CertificateInfo.getSubjectFields(cert).getField("CN"));
+        if (pkcs7.getSignName() != null)
+            System.out.println("Alternative name of the signer: " + pkcs7.getSignName());
+        SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println("Signed on: " + date_format.format(pkcs7.getSignDate().getTime()));
+        if (pkcs7.getTimeStampDate() != null) {
+            System.out.println("TimeStamp: " + date_format.format(pkcs7.getTimeStampDate().getTime()));
+            TimeStampToken ts = pkcs7.getTimeStampToken();
+            System.out.println("TimeStamp service: " + ts.getTimeStampInfo().getTsa());
+            System.out.println("Timestamp verified? " + pkcs7.verifyTimestampImprint());
+        }
+        System.out.println("Location: " + pkcs7.getLocation());
+        System.out.println("Reason: " + pkcs7.getReason());*/
+        PdfDictionary sigDict = signUtil.getSignatureDictionary(name);
+        PdfString contents = sigDict.getAsString(PdfName.Contents);
+        if (contents != null)
+            System.out.println("Contact info: " );
+
+        CertificateFactory certificateFactory = new CertificateFactory();
+        X509Certificate certificate = (X509Certificate) certificateFactory.engineGenerateCertificate(new ByteArrayInputStream(contents.getValueBytes()));
+        Principal subjectDN = certificate.getSubjectDN();
+        System.out.println("subjectDN:" + subjectDN);
+        System.out.println("IssuerDN:" + certificate.getIssuerDN());
+        System.out.println("SerialNumber:" + certificate.getSerialNumber());
+        System.out.println("SubjectCN:" + certificate.getIssuerDN());
+        System.out.println("SigningTime:" + PdfDate.getW3CDate(sigDict.getAsString(PdfName.M).toString()));
+
+        perms = new SignaturePermissions(sigDict, perms);
+        System.out.println("Signature type: " + (perms.isCertification() ? "certification" : "approval"));
+        System.out.println("Filling out fields allowed: " + perms.isFillInAllowed());
+        System.out.println("Adding annotations allowed: " + perms.isAnnotationsAllowed());
+        for (SignaturePermissions.FieldLock lock : perms.getFieldLocks()) {
+            System.out.println("Lock: " + lock.toString());
+        }
+        return perms;
+    }
+
+    public PdfPKCS7 verifySignature(SignatureUtil signUtil, String name) throws GeneralSecurityException, IOException {
+        System.out.println("Signature covers whole document: " + signUtil.signatureCoversWholeDocument(name));
+        System.out.println("Document revision: " + signUtil.getRevision(name) + " of " + signUtil.getTotalRevisions());
+        PdfPKCS7 pkcs7 = signUtil.verifySignature(name);
+        System.out.println("Integrity check OK? " + pkcs7.verify());
+        return pkcs7;
+    }
+
+    @Test
+    public void test2() throws FileNotFoundException, CertificateException {
+        String path = "D:\\wangchen\\tools/ks";
+        FileInputStream is = new FileInputStream(path);
+        CertificateFactory cf = new CertificateFactory();
+        X509Certificate certificate = (X509Certificate) cf.engineGenerateCertificate(is);
+        Principal subjectDN = certificate.getSubjectDN();
+        System.out.println("subjectDN:" + subjectDN);
+        System.out.println("IssuerDN:" + certificate.getIssuerDN());
+        System.out.println("SerialNumber:" + certificate.getSerialNumber());
+        System.out.println("SubjectCN:" + certificate.getIssuerDN());
+    }
+
+    @Test
+    public void addNewPage(){
+        String srcPath = "d:/tmp/hello.pdf";
+        String destPath = "d:/tmp/signatures/2.pdf";
+        try {
+            PdfDocument pdfDocument = new PdfDocument(new PdfReader(srcPath), new PdfWriter(destPath));
+            pdfDocument.addNewPage();
+            pdfDocument.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test3(){
+        int i = 5/3;
+        System.out.println(i);
+    }
 }
