86008ef385794bc546ed24d72533ad8499b5d3c6
wangchen <516760573@qq.com>
2019-07-16 11:13:06 +0800

    java concurrent

---
+++ java8-demo/src/main/java/com/mk/demos/java/concurrent/SynchronizedTest.java
@@ -0,0 +1,56 @@
+package com.mk.demos.java.concurrent;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * synchronized test
+ *
+ * synchronized(this) 和 synchronized方法都是锁定当前对象
+ * @author WangChen
+ * Created on 2019/4/29 11:27
+ * @since
+ */
+public class SynchronizedTest {
+
+    private int count = 0;
+    private Object obj = new Object();
+
+    public void testSync1(){
+        synchronized (obj){
+            System.out.println(Thread.currentThread().getName() + ",count = " + count++);
+        }
+    }
+
+    public void testSync2(){
+        synchronized (this){
+            System.out.println(Thread.currentThread().getName() + ",count = " + count++);
+        }
+    }
+
+    public synchronized void testSync3(){
+        System.out.println(Thread.currentThread().getName() + ",count = " + count++);
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        SynchronizedTest test1 = new SynchronizedTest();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                test1.testSync2();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                test1.testSync2();
+            }
+        }).start();
+    }
+}
