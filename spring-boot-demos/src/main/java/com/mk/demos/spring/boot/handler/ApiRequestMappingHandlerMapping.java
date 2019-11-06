503b393f0ba1b12220ffabf6d697cff2bea4bd19
wangchen <516760573@qq.com>
2019-11-06 18:31:31 +0800

    requestMappingHandlerMapping

--- spring-boot-demos/src/main/java/com/mk/demos/spring/boot/handler/ApiRequestMappingHandlerMapping.java
+++ spring-boot-demos/src/main/java/com/mk/demos/spring/boot/handler/ApiRequestMappingHandlerMapping.java
@@ -1,10 +1,19 @@
 package com.mk.demos.spring.boot.handler;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.condition.RequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import com.mk.demos.spring.boot.annotation.ApiVersion;
@@ -61,5 +70,40 @@ public class ApiRequestMappingHandlerMapping extends RequestMappingHandlerMappin
 
     private ApiVersionCondition buildFrom(ApiVersion apiVersion) {
         return apiVersion == null ? new ApiVersionCondition("1.5") : new ApiVersionCondition(apiVersion.value());
+    }
+
+    private final static Map<HandlerMethod, RequestMappingInfo> HANDLER_METHOD_REQUEST_MAPPING_INFO_MAP = new HashMap<>();
+
+    // 用于保存处理方法和RequestMappingInfo的映射关系(这个方法在解析@RequestMapping时就会被调用, 达达科技中这个地方可能写的有问题, 文中提到覆写AbstractHandlerMethodMapping#registerMapping方法, 但是经过实验之后覆写这个方法不能生效)
+    @Override
+    protected void registerHandlerMethod(Object handler, Method method, RequestMappingInfo mapping) {
+        HandlerMethod handlerMethod = super.createHandlerMethod(handler, method);
+        HANDLER_METHOD_REQUEST_MAPPING_INFO_MAP.put(handlerMethod, mapping);
+        super.registerHandlerMethod(handler, method, mapping);
+    }
+
+    @Override
+    protected HandlerMethod lookupHandlerMethod(String lookupPath, HttpServletRequest request) throws Exception {
+
+        // 判断请求参数中是否带了event字段
+        String event = request.getParameter("event");
+
+        // 如果没有带则说明这次的请求不带路径参数, 则使用默认的处理
+        if(StringUtils.isEmpty(event)) {
+            return super.lookupHandlerMethod(lookupPath, request);
+        }
+
+        // 如果带了, 则从Map(这个Map中的entry在后面介绍)中获取处理当前url的方法
+        List<HandlerMethod> handlerMethods = super.getHandlerMethodsForMappingName(event);
+        if(CollectionUtils.isEmpty(handlerMethods)) throw new RuntimeException("没有找到指定的方法");
+        if(handlerMethods.size() > 1) throw new RuntimeException("存在多个匹配的方法");
+
+        HandlerMethod handlerMethod = handlerMethods.get(0);
+
+        // 根据处理方法查找RequestMappingInfo, 用于解析路径url中的参数
+        RequestMappingInfo requestMappingInfo = HANDLER_METHOD_REQUEST_MAPPING_INFO_MAP.get(handlerMethod);
+        if(requestMappingInfo == null) throw new RuntimeException("没有对应的匹配方法");
+        super.handleMatch(requestMappingInfo, lookupPath, request);
+        return handlerMethod;
     }
 }
