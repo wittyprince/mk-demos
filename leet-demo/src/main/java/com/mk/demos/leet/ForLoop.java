d7d8e230649ff602659cb2b9756202f85b6bb3be
wangchen <516760573@qq.com>
2019-11-28 18:46:53 +0800

    for loop i++ vs ++i

---
+++ leet-demo/src/main/java/com/mk/demos/leet/ForLoop.java
@@ -0,0 +1,49 @@
+package com.mk.demos.leet;
+
+/**
+ * for loop i++ vs ++i
+ *
+ * @author WangChen
+ * Created on 2019/11/28 18:43
+ * @since 1.0
+ */
+public class ForLoop {
+
+    public static void main(String[] args){
+        new ForLoop().loop4();
+    }
+
+    private void loop1(){
+        int i;
+        for (i=3; i < 5; i++){
+            System.out.println(i);
+        }
+        System.out.println(i);
+    }
+
+    private void loop2(){
+        int i;
+        for (i=3; i < 5; ++i){
+            System.out.println(i);
+        }
+        System.out.println(i);
+    }
+
+    private void loop3(){
+        int i;
+        int n = 0;
+        for (i=3; n < 5; n = i++){
+            System.out.println(i);
+        }
+        System.out.println(i);
+    }
+
+    private void loop4(){
+        int i;
+        int n = 0;
+        for (i=3; n < 5; n = ++i){
+            System.out.println(i);
+        }
+        System.out.println(i);
+    }
+}
