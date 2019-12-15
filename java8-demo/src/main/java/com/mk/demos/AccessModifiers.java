0544357433587dfb8c3d09295ba71a0cbf48f7a2
wangchen <516760573@qq.com>
2019-12-15 09:26:21 +0800

    访问控制符 public protected private no_modifier

---
+++ java8-demo/src/main/java/com/mk/demos/AccessModifiers.java
@@ -0,0 +1,29 @@
+package com.mk.demos;
+
+/**
+ * access modifiers
+ *
+ * public, protected, private and default (package private)
+ *
+ * ______________________________________________________________
+ * |           │ Class │ Package │ Subclass │ Subclass │ World  |
+ * |           │       │         │(same pkg)│(diff pkg)│        |
+ * |───────────┼───────┼─────────┼──────────┼──────────┼────────|
+ * |public     │   +   │    +    │    +     │     +    │   +    |
+ * |───────────┼───────┼─────────┼──────────┼──────────┼────────|
+ * |protected  │   +   │    +    │    +     │     +    │        |
+ * |───────────┼───────┼─────────┼──────────┼──────────┼────────|
+ * |no modifier│   +   │    +    │    +     │          │        |
+ * |───────────┼───────┼─────────┼──────────┼──────────┼────────|
+ * |private    │   +   │         │          │          │        |
+ * |___________|_______|_________|__________|__________|________|
+ *  + : accessible         blank : not accessible
+ *
+ * @author WangChen
+ * Created on 2019/12/15 9:06
+ * @since 1.0
+ */
+public class AccessModifiers {
+
+
+}
