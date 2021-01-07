14ba34134c3ec23ef59d33fbaf077b0be9be5ef8
wangchen <516760573@qq.com>
2021-01-07 21:44:56 +0800

    特殊方式:通过java ServiceLoader方式加载Bean

---
+++ spring-demos/spring-beans/src/main/java/com/mk/demos/spring/beans/SpecialBeanInstantiationDemo.java
@@ -0,0 +1,31 @@
+package com.mk.demos.spring.beans;
+
+import java.util.ServiceLoader;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mk.demos.spring.beans.factory.AddressFactory;
+
+/**
+ * 特殊方式 Bean 实例化
+ *
+ * @author WangChen
+ * Created on 2021/1/7 21:30
+ * @since 1.0
+ */
+public class SpecialBeanInstantiationDemo {
+
+    public static void main(String[] args) {
+        String configLocation = "classpath:/META-INF/special-bean-instantiation-context.xml";
+        // 创建BeanFactory容器
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+        // 注册Configuration Class 配置类
+
+
+        ServiceLoader<AddressFactory> serviceLoader = ServiceLoader.load(AddressFactory.class, Thread.currentThread().getContextClassLoader());
+        for (AddressFactory addressFactory : serviceLoader) {
+            System.out.println(addressFactory.createAddress());
+        }
+    }
+}
---
+++ spring-demos/spring-beans/src/main/java/com/mk/demos/spring/beans/factory/DefaultAddressFactory2.java
@@ -0,0 +1,11 @@
+package com.mk.demos.spring.beans.factory;
+
+/**
+ * AddressFactory默认实现
+ *
+ * @author WangChen
+ * Created on 2021/1/7 21:07
+ * @since 1.0
+ */
+public class DefaultAddressFactory2 implements AddressFactory{
+}
