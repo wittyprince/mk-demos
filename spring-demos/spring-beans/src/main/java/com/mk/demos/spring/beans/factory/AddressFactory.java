ceff3c12a316129ec35b451c36387f67eefe235a
wangchen <516760573@qq.com>
2021-01-07 21:09:38 +0800

    实例化bean第三种方式：通过Bean工厂方法

---
+++ spring-demos/spring-beans/src/main/java/com/mk/demos/spring/beans/factory/AddressFactory.java
@@ -0,0 +1,17 @@
+package com.mk.demos.spring.beans.factory;
+
+import com.mk.demos.spring.beans.domain.Address;
+
+/**
+ * address 工厂类
+ *
+ * @author WangChen
+ * Created on 2021/1/7 20:55
+ * @since 1.0
+ */
+public interface AddressFactory {
+
+    default Address createAddress(){
+        return Address.createAddress();
+    }
+}
