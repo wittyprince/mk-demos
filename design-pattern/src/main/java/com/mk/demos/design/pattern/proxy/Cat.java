7b889f45917a67dcda1139be8cba53187d2ba960
wangchen <516760573@qq.com>
2021-01-17 12:51:13 +0800

    设计模式 design pattern：代理模式

---
+++ design-pattern/src/main/java/com/mk/demos/design/pattern/proxy/Cat.java
@@ -0,0 +1,15 @@
+package com.mk.demos.design.pattern.proxy;
+
+/**
+ * 猫
+ *
+ * @author WangChen
+ * Created on 2021/1/15 21:24
+ * @since 1.0
+ */
+public class Cat {
+
+    public void bark(){
+        System.out.println("Cat bark...");
+    }
+}
