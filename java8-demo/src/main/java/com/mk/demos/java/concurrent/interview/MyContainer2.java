8e093164d82c14c3572b65b24f0138e17623a6a7
wangchen <516760573@qq.com>
2021-03-02 22:05:38 +0800

    容器面试题

---
+++ java8-demo/src/main/java/com/mk/demos/java/concurrent/interview/MyContainer2.java
@@ -0,0 +1,11 @@
+package com.mk.demos.java.concurrent.interview;
+
+/**
+ * 写一个固定容量容器，拥有put和get方法，以及getCount方法
+ * 能够支持2个生产者线程以及10个消费者线程的阻塞调用
+ * @author WangChen
+ * Created on 2021/3/2 22:02
+ * @since 1.0
+ */
+public class MyContainer2 {
+}
