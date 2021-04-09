a3e9398de362a6255f81acb08953ed5276725ed7
wangchen <516760573@qq.com>
2021-04-09 15:34:11 +0800

    spring-ioc-container xml bean definition reader

---
+++ spring-demos/spring-ioc-container/src/main/java/com/mk/demos/spring/ioc/container/xml/XmlBeanDefinitionReaderTest.java
@@ -0,0 +1,28 @@
+package com.mk.demos.spring.ioc.container.xml;
+
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.GenericApplicationContext;
+
+import com.mk.demos.spring.model.User;
+
+/**
+ * xml bean definition reader
+ *
+ * @author WangChen
+ * Created on 2021/4/9 15:28
+ * @since 1.0
+ */
+public class XmlBeanDefinitionReaderTest {
+
+    public static void main(String [] args){
+        String configLocation = "META-INF/xml-context.xml";
+        GenericApplicationContext context = new GenericApplicationContext();
+        new XmlBeanDefinitionReader(context).loadBeanDefinitions(configLocation);
+        // 启动spring应用上下文
+        context.refresh();
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+        // 关闭spring应用上下文
+        context.close();
+    }
+}
