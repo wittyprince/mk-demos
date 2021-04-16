db36e78b504c162c9791a11c8632e09250a93dd1
wangchen <516760573@qq.com>
2021-04-16 19:59:45 +0800

    spring-event 注释掉BlackListEventPublisher, 以免产生歧义

--- spring-demos/spring-event/src/main/java/com/mk/demos/spring/event/BlackListEventPublisher.java
+++ spring-demos/spring-event/src/main/java/com/mk/demos/spring/event/BlackListEventPublisher.java
@@ -11,35 +11,35 @@ import org.springframework.context.ApplicationEventPublisherAware;
  * Created on 2021/4/16 14:07
  * @since 1.0
  */
-public class BlackListEventPublisher implements ApplicationEventPublisherAware, ApplicationEventPublisher {
-
-    private ApplicationEventPublisher eventPublisher;
-
-    /**
-     * At configuration time, the Spring container will
-     * detect that EmailService implements ApplicationEventPublisherAware
-     * and will automatically call setApplicationEventPublisher().
-     * In reality, the parameter passed in will be the Spring container itself;
-     * you’re simply interacting with the application context
-     * via its ApplicationEventPublisher interface.
-     *
-     * 实现 *Aware接口实现自动注入原理：
-     * 实现 *Aware接口的class bean，spring容器会自动call调用setXXX方法，
-     * 然后把spring容器自身当成参数传过去
-     */
-    @Override
-    public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
-        this.eventPublisher = publisher;
-    }
-
-
-    @Override
-    public void publishEvent(ApplicationEvent event) {
-        this.eventPublisher.publishEvent(event);
-    }
-
-    @Override
-    public void publishEvent(Object event) {
-        throw new RuntimeException("not support");
-    }
-}
+//public class BlackListEventPublisher implements ApplicationEventPublisherAware/*, ApplicationEventPublisher*/ {
+//
+//    private ApplicationEventPublisher eventPublisher;
+//
+//    /**
+//     * At configuration time, the Spring container will
+//     * detect that EmailService implements ApplicationEventPublisherAware
+//     * and will automatically call setApplicationEventPublisher().
+//     * In reality, the parameter passed in will be the Spring container itself;
+//     * you’re simply interacting with the application context
+//     * via its ApplicationEventPublisher interface.
+//     *
+//     * 实现 *Aware接口实现自动注入原理：
+//     * 实现 *Aware接口的class bean，spring容器会自动call调用setXXX方法，
+//     * 然后把spring容器自身当成参数传过去
+//     */
+//    @Override
+//    public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+//        this.eventPublisher = publisher;
+//    }
+//
+//
+////    @Override
+////    public void publishEvent(ApplicationEvent event) {
+////        this.eventPublisher.publishEvent(event);
+////    }
+////
+////    @Override
+////    public void publishEvent(Object event) {
+////        throw new RuntimeException("not support");
+////    }
+//}
