133a699883dfd7e8cd661fcb0c940914f9e0a014
wangchen <516760573@qq.com>
2021-04-20 10:30:13 +0800

    spring-hibernate 增加 Account sql

--- spring-demos/spring-hibernate/doc/hibernate-ddl.sql
+++ spring-demos/spring-hibernate/doc/hibernate-ddl.sql
@@ -4,3 +4,10 @@ CREATE TABLE `T_Hibernate_User`(
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE `T_Hibernate_Account`(
+    `id`   int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(64) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
