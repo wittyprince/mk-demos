75284f6a5dd6bace61671e9843bac496d7219184
wangchen <516760573@qq.com>
2021-07-10 22:39:54 +0800

    Java8 Lambda - FunctionTest

--- java8-demo/src/main/java/com/mk/demos/java8/lambda/FunctionalProgramming/FunctionTest.java
+++ java8-demo/src/main/java/com/mk/demos/java8/lambda/FunctionalProgramming/FunctionTest.java
@@ -1,6 +1,6 @@
 package com.mk.demos.java8.lambda.FunctionalProgramming;
 
-import java.awt.*;
+import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
 
 /**
@@ -15,5 +15,8 @@ public class FunctionTest {
     public static void main(String [] args){
         Predicate<Integer> p = x -> x > 5; // 可以看做相当于Holder的概念
 //        MyPredicate<Integer> m = x -> x > 5; // 无法编译通过
+
+//        BinaryOperator operator1 = (x, y) -> x + y; // 编译不通过, 需指明泛型信息
+        BinaryOperator<Long> operator2 = (x, y) -> x + y;
     }
 }
