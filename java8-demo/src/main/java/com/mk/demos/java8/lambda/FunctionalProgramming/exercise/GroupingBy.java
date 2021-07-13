6bd1fdaa2ec60022d26ea915e17a9bfa83171b6b
wangchen <516760573@qq.com>
2021-07-13 23:50:23 +0800

    Java8 Lambda - chapter5-GroupingBy收集器实现Collectors.groupingBy

--- java8-demo/src/main/java/com/mk/demos/java8/lambda/FunctionalProgramming/exercise/GroupingBy.java
+++ java8-demo/src/main/java/com/mk/demos/java8/lambda/FunctionalProgramming/exercise/GroupingBy.java
@@ -1,5 +1,8 @@
 package com.mk.demos.java8.lambda.FunctionalProgramming.exercise;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -10,7 +13,7 @@ import java.util.function.Supplier;
 import java.util.stream.Collector;
 
 /**
- * 第五章的GroupBying
+ * 第五章的自定义GroupingBy收集器实现Collectors.groupingBy功能
  *
  * @author WangChen
  * Created on 2021/7/12 10:13
@@ -18,28 +21,61 @@ import java.util.stream.Collector;
  */
 public class GroupingBy<T, K> implements Collector<T, Map<K, List<T>>, Map<K, List<T>>>{
 
+    private final static Set<Characteristics> characteristics = new HashSet<>();
+    static {
+        characteristics.add(Characteristics.IDENTITY_FINISH);
+    }
+
+    private final Function<? super T, ? extends K> classifier;
+    public GroupingBy(Function<? super T, ? extends K> classifier) {
+        this.classifier = classifier;
+    }
+
     @Override
     public Supplier<Map<K, List<T>>> supplier() {
-        return null;
+        return () -> new HashMap<>();
     }
 
     @Override
     public BiConsumer<Map<K, List<T>>, T> accumulator() {
-        return null;
+        return (acc, t) -> {
+            K k = classifier.apply(t);
+            List<T> ts = acc.get(k);
+            if (ts == null){
+                ts = new ArrayList<>();
+            }
+            ts.add(t);
+            acc.put(k, ts);
+        };
     }
 
     @Override
     public BinaryOperator<Map<K, List<T>>> combiner() {
-        return null;
+//        return (x, y) -> {
+//            x.putAll(y); // 使用putAll是错误的
+//            return x;
+//        };
+        return (m1, m2) -> {
+            m2.forEach(
+                    (k2, v2) -> {
+                        m1.merge(k2, v2,
+                                (value1, value2) -> {
+                                    value1.addAll(value2);
+                                    return value1;
+                                });
+                    }
+            );
+            return m1;
+        };
     }
 
     @Override
     public Function<Map<K, List<T>>, Map<K, List<T>>> finisher() {
-        return null;
+        return a -> a;
     }
 
     @Override
     public Set<Characteristics> characteristics() {
-        return null;
+        return characteristics;
     }
 }
