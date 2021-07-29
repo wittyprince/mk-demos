394acb7dbbd5b5c2c736bf356f667a8c3f3c2a9c
wangchen <516760573@qq.com>
2021-07-29 16:19:38 +0800

    mapstruct - 多个source参数 映射到 一个object中

--- mapstruct-demos/src/main/java/com/mk/demos/mapstruct/Person.java
+++ mapstruct-demos/src/main/java/com/mk/demos/mapstruct/Person.java
@@ -8,4 +8,14 @@ package com.mk.demos.mapstruct;
  * @since 0.1
  */
 public class Person {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
