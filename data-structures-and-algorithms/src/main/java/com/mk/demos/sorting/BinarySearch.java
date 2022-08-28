5c54c71cbab094daac2bc4e1ce77e715e8dfecb3
wangchen <516760573@qq.com>
2022-08-29 00:49:25 +0800

    algorithm-二分查找

---
+++ data-structures-and-algorithms/src/main/java/com/mk/demos/sorting/BinarySearch.java
@@ -0,0 +1,62 @@
+package com.mk.demos.sorting;
+
+import java.util.Arrays;
+
+/**
+ * 二分查找
+ *
+ * @author WangChen
+ * Created on 2022/8/28
+ * @since 1.0
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] a3 = {0, 1, 2, 3, 4, 5, 6, 7};
+        int target = 8;
+        binary(a3, target);
+    }
+
+    public static void binary(int[] arr, int target) {
+        Arrays.sort(arr);
+        if (target < arr[0] || target > arr[arr.length - 1]) {
+            System.out.println("not existing...");
+            return;
+        }
+        int high = arr.length, low = 0, cursor;
+        while (low < high) {
+            cursor = (low + high) / 2;
+            if (arr[cursor] > target) {
+                high = cursor;
+            }
+            if (arr[cursor] < target) {
+                low = cursor;
+            }
+            if (arr[cursor] == target) {
+                System.out.println(cursor);
+                break;
+            }
+        }
+
+
+    }
+
+    public static void recurse(int[] arr, int target) {
+        int mid = arr.length/2;
+//        if (mid < 0) {
+//            System.out.println("null");
+//            return;
+//        }
+        if (arr[mid] == target) {
+            //TODO 这里输出的mid是有误的，因为每次递归后数组变了，其下标指针也就变了
+            System.out.println(mid);
+            return;
+        }
+        if (arr[mid] > target) {
+            binary(Arrays.copyOf(arr, mid), target);
+        }
+        if (arr[mid] < target) {
+            binary(Arrays.copyOfRange(arr, mid, arr.length), target);
+        }
+    }
+}
