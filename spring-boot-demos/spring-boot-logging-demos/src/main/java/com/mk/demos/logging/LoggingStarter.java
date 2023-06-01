44517c4d591f989c69bd29476e05f56a69d9d48e
wangchen <516760573@qq.com>
2023-06-01 18:46:11 +0800

    spring-boot-logging-demos: spring boot 日志配置

---
+++ spring-boot-demos/spring-boot-logging-demos/src/main/java/com/mk/demos/logging/LoggingStarter.java
@@ -0,0 +1,16 @@
+package com.mk.demos.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+
+@SpringBootApplication
+public class LoggingStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingStarter.class, args);
+    }
+}
---
+++ spring-boot-demos/spring-boot-logging-demos/src/main/java/com/mk/demos/logging/controller/LoggingController.java
@@ -0,0 +1,18 @@
+package com.mk.demos.logging.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * logging controller
+ *
+ * @author WangChen
+ * Created on 2023/6/1
+ * @since 1.0
+ */
+@RestController
+@RequestMapping("/logging")
+public class LoggingController {
+
+
+}
