8598c8800695323841634d46fb06063b4ba7d878
wangchen <516760573@qq.com>
2023-06-05 15:58:19 +0800

    spring-boot-logging-demos: logback.xml日志脱敏

--- spring-boot-demos/spring-boot-logging-demos/src/main/java/com/mk/demos/logging/controller/LoggingController.java
+++ spring-boot-demos/spring-boot-logging-demos/src/main/java/com/mk/demos/logging/controller/LoggingController.java
@@ -1,6 +1,9 @@
 package com.mk.demos.logging.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,5 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/logging")
 public class LoggingController {
 
+    private final Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
+    @RequestMapping("/logging")
+    public String logging(@RequestParam("message") String message) {
+        logger.trace("===trace==={}", message);
+        logger.debug("===debug==={}", message);
+        logger.info("===info==={}", message);
+        logger.warn("===warn==={}", message);
+        logger.error("===error==={}", message);
+        logger.info(message);
+        return "logging";
+    }
 }
