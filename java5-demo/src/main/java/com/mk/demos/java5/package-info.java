c41fc2b321621bfa2316ad85304020b82742923b
wangchen <516760573@qq.com>
2024-04-18 09:42:01 +0800

    java5-demo: 用于探究早起JDK版本的不同, 如JDBC等

---
+++ java5-demo/src/main/java/com/mk/demos/java5/package-info.java
@@ -0,0 +1,8 @@
+package com.mk.demos.java5;
+
+// java5-demo: 用于探究早起JDK版本的不同, 如
+// JDBC3.0 was introduced with JDK1.4,
+// JDBC4.0 was introduced with JDK1.6,
+// JDBC4.1 was introduced with JDK1.7,
+// JDBC4.2 was introduced with JDK1.8,
+// JDBC4.3 was introduced with JDK1.9
