0c016a4bb8acd484314bb803665d0a4d6cd00d6d
wangchen <516760573@qq.com>
2024-04-19 14:02:43 +0800

    spring-demos: spring-exception 增加注释

--- spring-demos/spring-exception/src/main/java/com/mk/demos/spring/exception/controller/ExceptionController.java
+++ spring-demos/spring-exception/src/main/java/com/mk/demos/spring/exception/controller/ExceptionController.java
@@ -12,16 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
  * Created on 2024/4/18
  * @since 1.0
  */
-@RestController
+// @RestController is a convenience annotation that is itself annotated with @Controller and @ResponseBody.
+// This annotation is applied to a class to mark it as a request handler.
+    // @Controller is a common annotation that is used to mark a class as Spring MVC Controller.
+    // @ResponseBody is a Spring annotation that binds a method return value to the web response body.
+// 由于项目使用的是spring-context.xml配置文件，所以这里不需要使用@RestController注解
+//@RestController
 @RequestMapping("/exception")
 public class ExceptionController {
+
+//    @Autowired
+//    private ExceptionService exceptionService;
 
     @RequestMapping("/test")
     public String test() {
         throw new CustomException("test exception");
     }
 
-    @ExceptionHandler({CustomException.class})
+    @ExceptionHandler({CustomException.class, Exception.class})
     public void handleException() {
         System.err.println("handle exception...");
     }
---
+++ spring-demos/spring-exception/src/main/java/com/mk/demos/spring/exception/service/ExceptionService.java
@@ -0,0 +1,17 @@
+//package com.mk.demos.spring.exception.service;
+//
+//import org.springframework.stereotype.Service;
+//
+///**
+// * Exception Service
+// *
+// * @author WangChen
+// * Created on 2024/4/19
+// * @since 1.0
+// */
+//public class ExceptionService {
+//
+//    public void test() {
+//        throw new RuntimeException("service test exception");
+//    }
+//}
