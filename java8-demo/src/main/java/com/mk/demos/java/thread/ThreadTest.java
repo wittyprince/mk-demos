3961bc557b6409cf364d943f776db4923acf2702
wangchen <516760573@qq.com>
2024-10-15 15:45:10 +0800

    java8-Thread-子线程异常，主线程不会中断运行

--- java8-demo/src/main/java/com/mk/demos/java/thread/ThreadTest.java
+++ java8-demo/src/main/java/com/mk/demos/java/thread/ThreadTest.java
@@ -1,5 +1,7 @@
 package com.mk.demos.java.thread;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * thread test
  *
@@ -17,6 +19,16 @@ public class ThreadTest {
             // 子线程异常，主线程不会中断运行
             throw new RuntimeException("a");
         }).start();
+
+        try {
+            // 两种方式sleep：
+            // 1、Thread.sleep(1000_0);
+//            Thread.sleep(1000_0);
+            // 2、TimeUnit.SECONDS.sleep(10);
+            TimeUnit.SECONDS.sleep(10); // 底层调用的还是Thread.sleep(ms, ns);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println("2...");
     }
 }
