69c5e9437ee850e6a76d4f97d296cca66678ae20
wangchen <516760573@qq.com>
2024-10-17 15:43:42 +0800

    java8-TransmittableThreadLocal

---
+++ java8-demo/src/main/java/com/mk/demos/java/thread/TtlTest.java
@@ -0,0 +1,16 @@
+package com.mk.demos.java.thread;
+
+/**
+ * TransmittableThreadLocal
+ *
+ * @author WangChen
+ * Created on 2024/10/17
+ * @since 1.0
+ */
+public class TtlTest {
+
+    // 参考: spring-boot-ttl-demos
+    public static void main(String[] args) {
+
+    }
+}
