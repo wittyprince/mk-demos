1498ae4ee8a7e729fc8f82478362ed8b0f736571
wangchen <516760573@qq.com>
2024-12-20 14:30:39 +0800

    data-structure-and-algorithm - shell sort

--- data-structures-and-algorithms/src/main/java/com/mk/demos/sorting/ShellSorting.java
+++ data-structures-and-algorithms/src/main/java/com/mk/demos/sorting/ShellSorting.java
@@ -12,7 +12,11 @@ import java.util.Arrays;
 public class ShellSorting {
 
     public static void main(String[] args) {
-
+        int[] a1 = {5, 1, 3, 2, 4};
+        int[] a2 = {5, 6, 1, 3, 2, 4};
+        int[] a3 = {1, 2, 0, 3, 4, 5};
+        int[] a4 = {1, 2, 1, 3, 4, 5};
+        shellSort(a1);
     }
 
     public static void shell(int[] arr, int gap) {
@@ -41,5 +45,25 @@ public class ShellSorting {
             }
         }
 
+    }
+
+    public static void shellSort(int[] a) {
+        int i, j, gap;// gap为步长，每次减为原来的一半。
+        for (gap = a.length / 2; gap >= 1; gap=gap/2) {
+            for (i = gap; i <= a.length - 1; i = i + gap) {
+                int tmp = a[i];
+                for (j = i - gap; j >= 0; j = j -gap) {
+                    if (a[j] > tmp) {
+                        a[j + gap] = a[j];
+                    } else {
+                        break;
+                    }
+                }
+                a[j + gap] = tmp;
+            }
+        }
+        for (int k = 0; k <= a.length - 1; k++) {
+            System.out.println(a[k]);
+        }
     }
 }
